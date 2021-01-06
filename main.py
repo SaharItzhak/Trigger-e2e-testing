c4b5611104ddd1a910b8459f2f5b2501336633de
Sahar Itzhak <sahar.itzhak@QUALITEST.LOCAL>
2021-01-06 17:46:27 +0200

    Change print message x4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 msg = """This will auto-trigger Pytest-e2e-testing program on Jenkins on each commit"""
 
-print("$$$$$$$")
+print("$$$$$$")
 print(msg)
-print("$$$$$$$")
+print("$$$$$$")
+print("1")
